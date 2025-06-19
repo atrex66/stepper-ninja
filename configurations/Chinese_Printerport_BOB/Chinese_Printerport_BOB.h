8c0147efb87d97ca7444a7aa8b1ec75a4c25d7b9
Zsolt Viola <52636107+atrex66@users.noreply.github.com>
2025-06-19 18:52:37 +0200

    Update Chinese_Printerport_BOB.h
    defined pullups for all open collector inputs.

--- configurations/Chinese_Printerport_BOB/Chinese_Printerport_BOB.h
+++ configurations/Chinese_Printerport_BOB/Chinese_Printerport_BOB.h
@@ -15,7 +15,7 @@
     #define enc_pins {}
 
     #define in_pins {PIN_14, PIN_15, PIN_16, PIN_29, PIN_31, PIN_32}
-    #define in_pullup {0, 0, 0, 0, 0, 0}
+    #define in_pullup {1, 1, 1, 1, 1, 1}
 
     #define spindle_encoder_index_GPIO PIN_32
     #define spindle_encoder_active_level high
